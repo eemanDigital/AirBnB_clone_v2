93120e53988a97de0c817b77e77cb51387cf2968
Lukman Asinmi <asinmilukman@gmail.com>
2023-04-06 07:28:15 -0700

    Tasks completed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,9 +6,9 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.210.195.100', '54.89.23.59']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
