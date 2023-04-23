c6a0673e452eed0845c7b1707a77b45a1202883e
Lukman Asinmi <eemandigitalconcep@gmail.com>
2023-04-23 15:47:41 +0000

    fixing error

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This script starts a flask web application"""
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -44,4 +44,5 @@ def print_template(n):
 
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
